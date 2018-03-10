be8143efa1751bee2ade4abcf86766b1862fcda5
jchuajy <j.chua.jy@gmail.com>
2018-03-10 21:09:17 +0800

    built movement function

--- js/assets.js
+++ js/assets.js
@@ -1,11 +1,11 @@
 //Establishing encounters
-var statusQuestion = ["Homework assignment!" + "\n" + "Do you stay and complete or hide til class is over?" + "\n" + "(Input stay or hide)",
+var worldEvent = ["Homework assignment!" + "\n" + "Do you stay and complete or hide til class is over?" + "\n" + "(Input stay or hide)",
 						"Lab work!" + "\n" + "Do you stay and complete or hide til class is over?" + "\n" + "(Input stay or hide)", 
 						"Lecture!" + "\n" + "Do you stay and complete or hide til class is over?" + "\n" + "(Input stay or hide)"];
-var fightQuestion = ["You have encountered Akira!" + "\n" + "Do you fight or run away?" + "\n" + "(Input fight or run)", 
+var fightEvent = ["You have encountered Akira!" + "\n" + "Do you fight or run away?" + "\n" + "(Input fight or run)", 
 						"You have encountered Nick!" + "\n" + "Do you fight or run away?" + "\n" + "(Input fight or run)", 
 						"You have encountered Ray!" + "\n" + "Do you fight or run away?" + "\n" + "(Input fight or run)"];
-var moveQuestion = ["Which direction would you like to move?" + "\n" + "(Input n, s, e or w)"];
+var moveEvent = [" <p> Which direction would you like to move? </p> <button id='moveNorth'>North</button> <br><button id='moveWest'>West</button> &emsp; <button id='moveEast'>East</button> <br> <button id='moveSouth'>South</button>"];
 
-
+//Text to display on game initialise
 var initialText = "<p> Welcome to Path of ... ! To begin, type in your name! <p> <input id='playerNameInput' type='text' value='Player One'> <button id='submitPlayerName' type='submit'>Submit</button>"
--- js/script.js
+++ js/script.js
@@ -35,22 +35,35 @@ window.onload = function() {
 
 //build map function
 	var buildMap = function() {
+		//clear map
+		$("#map").html("");
+		//for length of map,
 		for (i = 0; i < __map.length; i++) {
+		//create an element <h1>
 		var maph1 = $('<h1>');
+		//concat the values of __map[i] and make a new line
 		var mapInitial = __map[i].join("     ") + "\n";
+		//add the concated string to <h1>
 		$("#map").append(maph1);
+		//append the <h1> to the map div
 		maph1.append(mapInitial);
 		};
 	};
 
+//change overlay content
+	var changeOverlayContent = function(content) {
+		$("#overlayContent").html("");
+		$("#overlayContent").html(content);
+	}
+
 //what happens upon on gamestart/game reset
 	var gameStart = function () {
 		//insert welcome text
-		$("#overlayContent").append(initialText);
+		changeOverlayContent(initialText);
 		//add event listener to submit button: get playername and close overlay
 		$("#submitPlayerName").click(function() {
 			__playerName = $("#playerNameInput").val();
-			closeOverlay();
+			newRoom();
 		});
 		//set current playerposition to 1-1
 		__currentPlayerRow = 0;
@@ -68,19 +81,68 @@ window.onload = function() {
 		__map[__endRow][__endCol] = "E";
 		openOverlay();
 		//create map
-		buildMap();
-		
+		buildMap();	
 	}
 
-//creation of events (which will run each time player enters new room)
-	var worldEvent = function() {
+//creation of world events (which will run each time player enters new room)
+	var exeWorldEvent = function() {
 		//show event text on main display
-		//
+		changeOverlayContent(worldEvent[Math.floor(Math.random() * worldEvent.length)]);
+
+	};
+
+//creation of fight battles (which will run each time player enters new room)
+	var exeFightEvent = function() {
+		//show event text on main display
+		changeOverlayContent(fightEvent[Math.floor(Math.random() * fightEvent.length)]);
 	}
 
+//creation of move events (which will run as the last event in any room)
+	var exeMoveEvent = function() {
+		//show event text on main display
+		changeOverlayContent(moveEvent[Math.floor(Math.random() * moveEvent.length)]);
+		$("#moveNorth").click(function() {movePlayerCheck("north");});
+		$("#moveSouth").click(function() {movePlayerCheck("south");});
+		$("#moveEast").click(function() {movePlayerCheck("east");});
+		$("#moveWest").click(function() {movePlayerCheck("west");});
 
 
+	}
 
+//check for move validity + change current player location
+	var movePlayerCheck = function(direction) {
+		if (direction === "north" && __currentPlayerRow === 0) {
+			alert("You can't move there!");
+		} else if (direction === "south" && __currentPlayerRow === (__map.length - 1)) {
+			alert("You can't move there!");
+		} else if (direction === "east" && __currentPlayerCol == (__map[0].length - 1)) {
+			alert("You can't move there!");
+		} else if (direction === "west" && __currentPlayerCol === 0) {
+			alert("You can't move there!");
+		} else if (direction == "north") {
+			__map[__currentPlayerRow][__currentPlayerCol] = ".";
+			__currentPlayerRow = __currentPlayerRow - 1;
+		} else if (direction == "south") {
+			__map[__currentPlayerRow][__currentPlayerCol] = ".";
+			__currentPlayerRow = __currentPlayerRow + 1;
+		} else if (direction == "east") {
+			__map[__currentPlayerRow][__currentPlayerCol] = ".";
+			__currentPlayerCol = __currentPlayerCol + 1;
+		} else if (direction == "west") {
+			__map[__currentPlayerRow][__currentPlayerCol] = ".";
+			__currentPlayerCol = __currentPlayerCol - 1;
+		} else {
+			return;
+		}
+		__map[__currentPlayerRow][__currentPlayerCol] = "X";
+		//show changes on map
+		buildMap();
+	}
+
+//things to execute when moving to new room
+	var newRoom = function() {
+		exeMoveEvent();
+	}
 
 
 
